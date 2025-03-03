1e7c17a35eb56de40b8bd972887005d153e66744
Moamen Hraden <moamen.hraden@primion.eu>
2025-03-03 12:37:34 +0100

    update build script - zig 0.13.0 expect root source file instead of a module for the   executable - define build_options module and add version to it

--- build.zig
+++ build.zig
@@ -1,23 +1,29 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    const zox_version: std.SemanticVersion = .{ .major = 0, .minor = 1, .patch = 0 };
+    const stringified_version: []const u8 = b.fmt("{d}.{d}.{d}", .{ zox_version.major, zox_version.minor, zox_version.patch });
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe_mod = b.createModule(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
     const exe = b.addExecutable(.{
         .name = "zox",
-        .root_module = exe_mod,
+        .root_source_file = b.path("src/main.zig"),
+        .optimize = optimize,
+        .target = target,
     });
+
+    // add build options module
+    const exe_options = b.addOptions();
+    exe.root_module.addOptions("build_options", exe_options);
+
+    // const version = b.allocator.dupeZ(u8, stringified_version);
+    exe_options.addOption([]const u8, "version", stringified_version);
 
     b.installArtifact(exe);
 
     const run_cmd = b.addRunArtifact(exe);
+
     run_cmd.step.dependOn(b.getInstallStep());
     if (b.args) |args| {
         run_cmd.addArgs(args);
@@ -27,7 +33,9 @@ pub fn build(b: *std.Build) void {
     run_step.dependOn(&run_cmd.step);
 
     const exe_unit_tests = b.addTest(.{
-        .root_module = exe_mod,
+        .root_source_file = b.path("src/main.zig"),
+        .optimize = optimize,
+        .target = target,
     });
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
