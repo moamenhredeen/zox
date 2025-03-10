27fcf7d534c1c848868fe6051a92561abe78101e
Moamen Hraden <moamen.hraden@primion.eu>
2025-03-10 23:04:17 +0100

    feature(refactor): utils module documentation

--- src/utils.zig
+++ src/utils.zig
@@ -1,6 +1,9 @@
+//! useful utilities
+
 const std = @import("std");
 
-// TODO: difference between noreturn and void
+/// panic exit with an error message
+/// log the error message and exit the program immediatly
 pub fn panic(comptime format: []const u8, args: anytype) noreturn {
     std.log.err(format, args);
     std.process.exit(1);
